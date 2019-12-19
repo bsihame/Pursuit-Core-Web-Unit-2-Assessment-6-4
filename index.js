9ba3e94d093cfc7acba56e27acec988bcf737400
bsihame <sihamebazi@pursuit.org>
2019-12-18 22:03:22 -0500

    add some css

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
+let data;
+let titles;
 document.addEventListener("DOMContentLoaded", () =>{
-  let data;
   //declare variables
   let select = document.querySelector("select")
-  //let info = document.querySelector("displayInfo")
+  let info = document.querySelector("displayInfo")
   let infoDiv = document.querySelector("displayInfo")
-    let titleMovie = document.querySelector("#title");
-    let year = document.querySelector("#year");
-    let description = document.querySelector("#description");
+  let titleMovie = document.querySelector("#title");
+  let year = document.querySelector("#year");
+  let description = document.querySelector("#description");
   // create a function to get all the titles  we gonna use axios we need async to get promise
   const getAllTitles = async()=> {
    
@@ -31,20 +32,49 @@ document.addEventListener("DOMContentLoaded", () =>{
     }
   }
   const displayFilmDetails = (title) => {
-    if (!title) {
+    if (title === "" && !title) {
       titleMovie.innerText = "";
       year.innerText= "";
       description.innerText = "";
     } else {
-      for(let i=0; i< titles.length; i++){
-        if (tittle === title[i].title){
-          year.innerText = title[i].release_date;
-          description.innerText = title[i].description;
-          titleMovie.innerText = title[i].title;
+      for (let i=0; i< titles.length; i++){
+        if (tittle){
+          year.innerText = titles[i].release_date;
+          description.innerText = titles[i].description;
+          titleMovie.innerText = titles[i].title;
+          
         }
       }
      
     }
   }
+  const addReviewDom = (review) => {
+    let titleMovie = document.querySelector("#title").innerText
+    if (titleMovie) {
+      let ul = document.querySelector("ul");
+      let li = document.createElement("li");
+      let boldText = document.createElement("strong");
+      let span = document.creatElement("span");
+      boldText.innerText = title + ": ";
+      li.appendChild(boldText);
+      span.innerText = review;
+      li.appendChild(li);
+      ul.appendChild(li);
+    }
+  }
+  select.addEventListener("change", (e) => {
+    displayFilmDetails(e.target.value)
+  });
+  let form = document.querySelector("form");
+  form.addEventListener("submit", (e) =>{
+    e.preventDefault();
+    let li = document.creatElement("li");
+    let input = document.querySelector("input");
+    li.innerText = input.value;
+    let ul = document.querySelector("ul");
+    ul.appendChild(li);
+    input.value = "";
+  })
+
   getAllTitles();
 })
