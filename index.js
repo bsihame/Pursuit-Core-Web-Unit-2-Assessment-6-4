8a06bcd0396f86db1fc4ada844229729f696947f
bsihame <sihamebazi@pursuit.org>
2020-01-04 09:56:01 -0500

    work perfect

--- index.js
+++ index.js
@@ -1,109 +1,32 @@
-// let data;
-// let titles;
-// document.addEventListener("DOMContentLoaded", () =>{
-//   //declare variables
-//   let select = document.querySelector("select")
-//   let info = document.querySelector("displayInfo")
-//   let infoDiv = document.querySelector("displayInfo")
-//   let titleMovie = document.querySelector("#title");
-//   let year = document.querySelector("#year");
-//   let description = document.querySelector("#description");
-//   // create a function to get all the titles  we gonna use axios we need async to get promise
-//   const getAllTitles = async()=> {
-   
-//     try{
-//       // grab data we want
-//       let data = await axios.get("https://ghibliapi.herokuapp.com/films");
-//       data = data.data
-//       titles = data
-//       //data is in array so we need to loop
-//       data.forEach(film => {
-//         let option = document.createElement('option')
-//         option.innerText = film.title;
-//         // titleMovie.innerText = film.title
-//         option.value = film.title
-//         //debugger
-//         select.appendChild(option)
-//       });
-      
-//     }catch(err){
-//       console.log(err)
-//     }
-//   }
-//   const displayFilmDetails = (getAllTitles) => {
-//     if (title === "" && !title) {
-//      titleMovie.innerText = "";
-//      year.innerText= "";
-//     description.innerText = "";
-//      debugger
-//   //   } else {
-//   //     for (let i=0; i< titles.length; i++){
-//   //       if (tittle){
-//   //         year.innerText = titles[i].release_date;
-//   //         description.innerText = titles[i].description;
-//   //         titleMovie.innerText = titles[i].title;
-//   //         debugger
-          
-//         }
-//   //     }
-     
-//   //   }
-//   }
-//   const addReviewDom = (review) => {
-//     let titleMovie = document.querySelector("#title").innerText
-//     if (titleMovie) {
-//       let ul = document.querySelector("ul");
-//       let li = document.createElement("li");
-//       // let boldText = document.createElement("strong");
-//       // let span = document.creatElement("span");
-//       //boldText.innerText = title + ": ";
-//       // li.appendChild(boldText);
-//       // span.innerText = review;
-//       // li.appendChild(li);
-//       // ul.appendChild(li);
-//       // titleMovie.appendChild("li");
-//       // ul.appendChild(li)
-//     }
-//   }
-//   select.addEventListener("change", (e) => {
-//    // displayFilmDetails(e.target.value)
-//    addReviewDom(e.target.value)
-//   });
-//   let form = document.querySelector("form");
-//   form.addEventListener("submit", (e) =>{
-//     e.preventDefault();
-//     let li = document.creatElement("li");
-//     let input = document.querySelector("input");
-//     li.innerText = input.value;
-//     let ul = document.querySelector("ul");
-//     ul.appendChild(li);
-//     input.value = "";
-//   })
-
-//   getAllTitles();
-
-// })
-
 let data;
 let titles;
 document.addEventListener("DOMContentLoaded", () =>{
   //declare variables
   let select = document.querySelector("select")
-  
+  let info = document.querySelector("displayInfo")
+  let infoDiv = document.querySelector("displayInfo")
+  let titleMovie = document.querySelector("#title");
+  let year = document.querySelector("#year");
+  let description = document.querySelector("#description");
+  let ul = document.querySelector("ul");
+  let input = document.querySelector("input");
   // create a function to get all the titles  we gonna use axios we need async to get promise
   const getAllTitles = async()=> {
-   
+
     try{
       // grab data we want
-      let data = await axios.get("https://ghibliapi.herokuapp.com/films");
-      data = data.data
-      titles = data
+      let res = await axios.get("https://ghibliapi.herokuapp.com/films");
+      data = res.data
+      console.log(data)
+      // debugger
       //data is in array so we need to loop
-      titles.forEach(film => {
+      data.forEach(film => {
         let option = document.createElement('option')
         option.innerText = film.title;
+        option.value = film.id;
         // titleMovie.innerText = film.title
-        option.value = film.title
+        // option.value = film.title
+        //debugger
         select.appendChild(option)
       });
       
@@ -111,10 +34,54 @@ document.addEventListener("DOMContentLoaded", () =>{
       console.log(err)
     }
   }
+  const displayFilmDetails = async (id) => {
+    if (!id) {
+     titleMovie.innerText = "";
+     year.innerText= "";
+    description.innerText = "";
+    } else {
+      try {
+        let res = await axios.get(`https://ghibliapi.herokuapp.com/films/${id}`)
+        titleMovie.innerText = res.data.title;
+        year.innerText= res.data.release_date;
+        description.innerText = res.data.description;
+      } catch (err) {
+        console.log(err)
+      }
+    }
+
+  }
+  const addReviewDom = (review) => {
+    let titleMovie = document.querySelector("#title").innerText
+    
+    if (titleMovie) {
+      let li = document.createElement("li");
+
+      let title = document.createElement('strong')
+      title.innerText = titleMovie + ': '
+      li.appendChild(title)
+
+      let rev = document.createElement('span')
+      rev.innerText = review
+      li.appendChild(rev)
+      
+      ul.appendChild(li);
+    }
+  }
   select.addEventListener("change", (e) => {
-    //displayFilmDetails(e.target.value)
-    debugger
+    displayFilmDetails(e.target.value)
   });
 
+
+  let form = document.querySelector("form");
+  form.addEventListener("submit", (e) =>{
+    e.preventDefault();
+
+    addReviewDom(input.value)
+    input.value = "";
+  })
+
   getAllTitles();
+
 })
+
